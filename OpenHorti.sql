f76946bb8197babad11e119de2d30bb536f88ab6
Marco <162582090+marccosabino@users.noreply.github.com>
2025-03-28 10:15:57 -0300

    Update OpenHorti.sql

--- OpenHorti.sql
+++ OpenHorti.sql
@@ -11,3 +11,51 @@ VALUES ('Abacate', '1632935', 'R$ 7,00/kg', 'abacate.jpg');
 
 INSERT INTO produtos (nome, codigo, preco, imagem)
 VALUES ('Avocado', '00000004814974', 'R$ 10,00/kg', 'avocado.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Ameixa Nacional', '00000001637932', 'R$ 8,00/kg', 'ameixaNacional.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Ameixa Importada', '00000005606912', 'R$ 12,00/kg', 'ameixaImportada.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Atemoia', '00000035858919', 'R$ 15,00/kg', 'atemoia.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Banana Nanica', '00000001643973', 'R$ 3,50/kg', 'bananaNanica.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Banana Prata', '00000001646951', 'R$ 4,00/kg', 'bananaPrata.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Banana Maçã', '00000001661910', 'R$ 4,50/kg', 'bananaMaca.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Banana Terra', '00000004808915', 'R$ 5,00/kg', 'bananaTerra.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Banana Pacovan', '00000012657986', 'R$ 5,50/kg', 'banana_pacovan.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Caqui Fuyu', '00000000021843', 'R$ 6,00/kg', 'bananaFuyu.png');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Caqui Rama Forte', '00000000021700', 'R$ 6,50/kg', 'caquiRamaForte.png');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Carambola', '00000009109927', 'R$ 7,00/kg', 'carambola.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Caju Tropical', '00000013080961', 'R$ 8,00/kg', 'cajuTropical.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Coco Seco Tropical', '00000001650967', 'R$ 6,00/un', 'cocoSeco.jpg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Goiaba Vermelha Tropical', '00000006609989', 'R$ 4,00/kg', 'goiabaVermelha.png');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Kiwi', '00000001788974', 'R$ 10,00/kg', 'kiwi.jpeg');
+
+INSERT INTO produtos (nome, codigo, preco, imagem)
+VALUES ('Kiwi Sun Gold', '00000073384961', 'R$ 12,00/kg', 'kiwisunglod.jpeg');
